2d160b7c2ce5765277185ccdc4eda3893c069192
CodeMalayalam <65383511+CodeMalayalam@users.noreply.github.com>
2020-05-15 15:22:17 +0530

    Add files via upload

---
+++ pattern2.c
@@ -0,0 +1,32 @@
+/* PRINTING THE FOLLOWING PATTERN
+ *
+ *   *
+ *   **
+ *   ***
+ *   ****
+ *   *****
+ *
+ * SUBSCRIBE TO CODEMALAYALAM YOUTUBE CHANNEL 
+ * THANK YOU
+ */
+
+#include<stdio.h>
+
+int main()
+{
+	int i, j, n;
+
+	scanf("%d",&n);
+	
+	for(i = 1; i <= n ; ++i)
+	{
+		for(j = 1 ; j <= i ; ++j)
+		{
+			printf(" * ");
+		}
+		printf("\n");
+	}
+
+	
+	return 0;
+}
