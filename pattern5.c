4e5eaff4f08af63f5ac2d561b4fa1118dc3f346d
CodeMalayalam <65383511+CodeMalayalam@users.noreply.github.com>
2020-05-29 14:24:04 +0530

    Add files via upload

---
+++ pattern5.c
@@ -0,0 +1,32 @@
+/*PRINT THE FOLLOWING PATTERN :
+ *  
+ *   *****
+ *    ****
+ *     ***
+ *      **
+ *       *
+ *
+ * PLEASE SUBSCRIBE TO CODEMALAYALAM YT CHANNEL
+ */
+
+#include<stdio.h>
+
+int main()
+{
+	int i, j, k, n;
+	
+	scanf("%d",&n);
+
+	for( i = 1 ; i <= n ; ++i )
+	{
+		for( j = 1 ; j < i ; ++j)
+			printf("   ");
+		for( k = i ; k <= n ; ++k)
+			printf(" * ");
+		printf("\n");
+		
+	}
+	return 0;
+	
+}
+
